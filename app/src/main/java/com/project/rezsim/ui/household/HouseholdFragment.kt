c5afa9a3d4e5a6f461c637fbee8244bf22ca6007
zoltan.halasz <zoltan.halasz@rrsoftware.hu>
2023-02-01 09:41:56 +0100

    ...

--- app/src/main/java/com/project/rezsim/ui/household/HouseholdFragment.kt
+++ app/src/main/java/com/project/rezsim/ui/household/HouseholdFragment.kt
@@ -1,5 +1,10 @@
 package com.project.rezsim.ui.household
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.ImageButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.widget.AppCompatSpinner
 import com.project.rezsim.R
 import com.project.rezsim.base.RezsimFragment
@@ -16,8 +21,13 @@ class HouseholdFragment : RezsimFragment() {
     private lateinit var spinnerUsage: AppCompatSpinner
     private lateinit var spinnerPricingTypeA: AppCompatSpinner
     private lateinit var spinnerPricingTypeB: AppCompatSpinner
+    private lateinit var buttonChildrenMinus: AppCompatImageButton
+    private lateinit var buttonChildrenPlus: AppCompatImageButton
+    private lateinit var editChildren: AppCompatEditText
+
 
     private val spinnerItemSelectedListener = TextSpinnerOnItemSelectedListener()
+
 
     override fun setupViews() {
         super.setupViews()
@@ -34,13 +44,31 @@ class HouseholdFragment : RezsimFragment() {
                 adapter = TextSpinnerAdapter(requireContext(), viewModel.pricingTypeBItems())
                 onItemSelectedListener = spinnerItemSelectedListener
             }
+            buttonChildrenMinus = it.findViewById<AppCompatImageButton>(R.id.btChildrenMinus).apply {
+                setOnClickListener { viewModel.childrenButtonClicked(editChildren.text.toString(), -1) }
+            }
+            buttonChildrenPlus = it.findViewById<AppCompatImageButton>(R.id.btChildrenPlus).apply {
+                setOnClickListener { viewModel.childrenButtonClicked(editChildren.text.toString(), 1) }
+            }
+            editChildren = it.findViewById<AppCompatEditText>(R.id.etChildrenCount).apply {
+                addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
+                    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
+                    override fun afterTextChanged(p0: Editable?) {
+                        val value = p0?.toString()?.toInt()
+                        buttonChildrenMinus.isEnabled = value != null && value > 0
+                    }
+                })
+            }
         }
-
-
     }
 
+    override fun subscribeObservers() {
+        super.subscribeObservers()
+        viewModel.childrenValueLiveData.observe(this) {editChildren.setText(it.toString())}
+    }
 
 
     companion object {
--- app/src/main/java/com/project/rezsim/ui/household/HouseholdViewModel.kt
+++ app/src/main/java/com/project/rezsim/ui/household/HouseholdViewModel.kt
@@ -1,8 +1,20 @@
 package com.project.rezsim.ui.household
 
+import androidx.annotation.IntegerRes
+import androidx.lifecycle.MutableLiveData
 import com.project.rezsim.base.RezsimViewModel
+import com.project.server.dto.Household
 
 class HouseholdViewModel : RezsimViewModel() {
+
+    val contentLiveData = MutableLiveData<Household?>()
+    val childrenValueLiveData = MutableLiveData<Int>()
+
+    private var houseHold: Household? = null
+    set(value) {
+        field = value
+        contentLiveData.postValue(value)
+    }
 
 
 
@@ -11,5 +23,12 @@ class HouseholdViewModel : RezsimViewModel() {
     fun pricingTypeAItems() = listOf("Kérem válasszon!", "Első elem", "Második elem", "Harmadik elem")
 
     fun pricingTypeBItems() = listOf("Kérem válasszon!", "Első elem", "Második elem", "Harmadik elem")
+
+    fun childrenButtonClicked(oldValue: String?, add: Int) {
+        val value = if (oldValue.isNullOrBlank()) 0 else oldValue.toInt()
+        childrenValueLiveData.value = value + add
+    }
+
+
 
 }
