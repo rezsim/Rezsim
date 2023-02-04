4766378109378b252cde84b3516b5a007c193f27
zoltan.halasz <zoltan.halasz@rrsoftware.hu>
2023-02-05 00:16:50 +0100

    ...

--- app/src/main/java/com/project/rezsim/device/ScreenRepository.kt
+++ app/src/main/java/com/project/rezsim/device/ScreenRepository.kt
@@ -1,10 +1,12 @@
 package com.project.rezsim.device
 
 import android.content.Context
+import android.content.res.Resources
 import com.project.rezsim.R
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import org.koin.java.KoinJavaComponent.inject
+import kotlin.math.roundToInt
 
 class ScreenRepository : KoinComponent {
 
@@ -13,3 +15,7 @@ class ScreenRepository : KoinComponent {
     fun isTablet() = context.resources.getBoolean(R.bool.is_tablet)
 
 }
+val Int.dp: Int
+    get() = (this * (Resources.getSystem().displayMetrics.densityDpi / 160f)).roundToInt()
+val Int.px: Int
+    get() = (this / (Resources.getSystem().displayMetrics.densityDpi / 160f)).roundToInt()
--- app/src/main/java/com/project/rezsim/ui/screen/main/MainFragment.kt
+++ app/src/main/java/com/project/rezsim/ui/screen/main/MainFragment.kt
@@ -1,12 +1,21 @@
 package com.project.rezsim.ui.screen.main
 
+import android.graphics.PorterDuff
+import android.text.Layout
+import android.view.Gravity
 import android.view.View
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatSpinner
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.content.res.ResourcesCompat
 import com.project.rezsim.R
 import com.project.rezsim.base.RezsimFragment
 import com.project.rezsim.device.ScreenRepository
+import com.project.rezsim.device.dp
 import com.project.rezsim.server.dto.Household
 import com.project.rezsim.ui.view.spinner.TextSpinnerAdapter
 import com.project.rezsim.ui.view.spinner.TextSpinnerOnItemSelectedListener
@@ -25,6 +34,15 @@ class MainFragment : RezsimFragment() {
         super.setupViews()
         view?.let {
             if (screenRepository.isTablet()) {
+                val layout: LinearLayoutCompat = it.findViewById(R.id.llHouseholds)
+                viewModel.householdItems().forEachIndexed { index, s ->
+                    if (index > 0) {
+                        val lp = ViewGroup.MarginLayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT).apply {
+                            setMargins(0, 0, 100, 0)
+                        }
+                        layout.addView(createHouseholdButton(index, s), lp)
+                    }
+                }
             } else {
                 spinnerHouseholds = it.findViewById<AppCompatSpinner?>(R.id.spHousehold).apply {
                     adapter = TextSpinnerAdapter(requireContext(), viewModel.householdItems())
@@ -39,6 +57,20 @@ class MainFragment : RezsimFragment() {
                 }
             }
         }
+    }
+
+    private fun createHouseholdButton(id: Int, text: String) = AppCompatButton(requireContext()).apply {
+        setId(id)
+        layoutParams =  ViewGroup.MarginLayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT).apply {
+            setMargins(0, 0, 100, 0)
+        }
+        background = ResourcesCompat.getDrawable(resources, R.drawable.statelist_button_background, null)
+        setText(text)
+        setTextColor(resources.getColor(R.color.button_text_color))
+        textAlignment = View.TEXT_ALIGNMENT_CENTER
+        setPadding(0, 0, 0, 0)
+        backgroundTintList = resources.getColorStateList(R.color.material_grey_8, null)
+        backgroundTintMode = PorterDuff.Mode.ADD
     }
 
     companion object {
