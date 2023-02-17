f643789e2d782c354bdc10a55a50712e2ce95810
zoltan.halasz <zoltan.halasz@rrsoftware.hu>
2023-02-17 22:07:41 +0100

    ...

--- app/src/main/java/com/project/rezsim/ui/screen/splash/SplashFragment.kt
+++ app/src/main/java/com/project/rezsim/ui/screen/splash/SplashFragment.kt
@@ -2,6 +2,9 @@ package com.project.rezsim.ui.screen.splash
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import com.project.rezsim.BuildConfig
 import com.project.rezsim.R
 import com.project.rezsim.base.RezsimFragment
 import org.koin.android.ext.android.inject
@@ -15,6 +18,14 @@ class SplashFragment : RezsimFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.start()
+    }
+
+    override fun setupViews() {
+        super.setupViews()
+        view?.let {
+            val ver = "v.${BuildConfig.VERSION_NAME}"
+            it.findViewById<AppCompatTextView>(R.id.tvVersion).text = ver
+        }
     }
 
     companion object {
